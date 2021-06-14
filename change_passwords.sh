9f9716cbfc0ae738b8a72ac69c7a1259f10f0b25
Joel Goehring <rsoc39@gmail.com>
2021-06-14 15:40:46 -0700

    Updated with License

--- change_passwords.sh
+++ change_passwords.sh
@@ -7,16 +7,36 @@
 #   Style Guide: https://github.com/bahamas10/bash-style-guide
 #######################################################################################################################
 
-###############################################-----DISCLAIMER-----####################################################
-#   You running this script/function means you will not blame the author(s) if this breaks your stuff.
-#   This script/function is provided AS IS without warranty of any kind. Author(s) disclaim all implied warranties
-#   including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
-#   The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
-#   In no event shall author(s) be held liable for any damages whatsoever (including, without limitation, damages
-#   for loss of business profits, business interruption, loss of business information, or other pecuniary loss)
-#   arising out of the use of or inability to use the script or documentation. Neither this script/function,
-#   nor any part of it other than those parts that are explicitly copied from others, may be republished
-#   without author(s) express written permission.
+###############################################-----LICENSE-----####################################################
+#   BSD 3-Clause License
+
+#   Copyright (c) 2021, Joel Goehring
+#   All rights reserved.
+
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are met:
+
+#   1. Redistributions of source code must retain the above copyright notice, this
+#        list of conditions and the following disclaimer.
+
+#   2. Redistributions in binary form must reproduce the above copyright notice,
+#       this list of conditions and the following disclaimer in the documentation
+#       and/or other materials provided with the distribution.
+
+#   3. Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #######################################################################################################################
 
 ###############################################-----PARAMETERS-----####################################################
